aa66cdc2d5c61115cc310c9570d6206a8c27179c
tishayaem <kostiantynkalinin@gmail.com>
2018-09-27 18:50:30 +0100

    Configure with parcel

---
+++ src/Pet.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const Pet = props => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h3", {}, props.breed)
+    ]);
+};
