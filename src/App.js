6dae7e383ef4def5e69929db66b865f623c00497
tishayaem <kostiantynkalinin@gmail.com>
2018-10-02 12:57:21 +0100

    Render animal name, breed and photo from API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import pf from "petfinder-client";
+import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { pets: [] };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Los Angeles, Ca" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({ pets });
+      });
+  }
   render() {
     return (
       <div>
         <h1>Adopter</h1>
-        <Pet name="Luna" animal="Dog" breed="Husky" />
-        <Pet name="Biscuit" animal="Cat" breed="Street" />
-        <Pet name="Ed" animal="Seal" breed="Blackanese" />
+        <div>
+          {this.state.pets.map(pet => {
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={pet.breeds.breed}
+                media={pet.media}
+                location={pet.contact.city}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
