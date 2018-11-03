6908e9f5da199639a641596ec4b5483acefb212b
tishayaem <kostiantynkalinin@gmail.com>
2018-11-03 15:12:55 +0000

    Install emotion and style Navbar component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "@reach/router";
+import styled from "react-emotion";
+
+const Container = styled("header")`
+  background-color: #333;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
 
 const NavBar = () => (
-  <header>
+  <Container>
     <Link to="/">Adopter</Link>
     <Link to="/search-params">
       <span aria-label="search" role="img">
         🔍
       </span>
     </Link>
-  </header>
+  </Container>
 );
 export default NavBar;
