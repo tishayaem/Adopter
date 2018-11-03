73bed6331c5b4974f1249d3f7da36feb2fd4ba62
tishayaem <kostiantynkalinin@gmail.com>
2018-11-03 22:36:11 +0000

    Add animation to NavBar with emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,60 @@
 import React from "react";
 import { Link } from "@reach/router";
-import styled from "react-emotion";
+import styled, { keyframes } from "react-emotion";
+import colours from "./colours";
 
+const Spin = keyframes`
+    from {
+        transform: rotate(0deg)
+    }
+
+    to {
+        transform: rotate(360deg)
+    }
+`;
+
+const SpyGlass = styled("span")`
+  display: inline-block;
+  animation: ${props => props.frequency}s ${Spin} linear infinite;
+`;
 const Container = styled("header")`
-  background-color: #333;
+  background-color: ${colours.dark};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
-const NavBar = () => (
-  <Container>
-    <Link to="/">Adopter</Link>
-    <Link to="/search-params">
-      <span aria-label="search" role="img">
-        🔍
-      </span>
-    </Link>
-  </Container>
-);
-export default NavBar;
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+  span {
+    padding: 10px;
+    border: 2px solid white;
+  }
+`;
+
+export default class NavBar extends React.Component {
+  state = {
+    frequency: 10
+  };
+
+  halfFrequency = () => this.setState({ frequency: this.state.frequency / 2 });
+  render() {
+    return (
+      <Container>
+        <NavLink to="/">Adopter</NavLink>
+        <NavLink to="/search-params">
+          <SpyGlass
+            onClick={this.halfFrequency}
+            frequency={this.state.frequency}
+            aria-label="search"
+            role="img"
+          >
+            🔍
+          </SpyGlass>
+        </NavLink>
+      </Container>
+    );
+  }
+}
